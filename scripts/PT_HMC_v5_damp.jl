82c7b796ef80562920af4475dae6afd0dce53247
Ludovic Raess <ludovic.rass@gmail.com>
2022-02-18 09:55:59 +0100

    Suggested changes - mostly about consistency.

--- scripts/PT_HMC_v5_damp.jl
+++ scripts/PT_HMC_v5_damp.jl
@@ -9,10 +9,18 @@ else
     @init_parallel_stencil(Threads, Float64, 2)
 end
 using Plots, Printf, Statistics, LinearAlgebra, MAT
-##################################################
-@views av_xy(A) = 0.25*(A[1:end-1,1:end-1].+A[2:end,1:end-1].+A[1:end-1,2:end].+A[2:end,2:end])
+
+import ParallelStencil: INDICES
+ix,iy   = INDICES[1], INDICES[2]
+ixi,iyi = :($ix+1), :($iy+1)
+
+"Average in x and y dimension"
+@views    av(A) = 0.25*(A[1:end-1,1:end-1].+A[2:end,1:end-1].+A[1:end-1,2:end].+A[2:end,2:end])
+"Average in x dimension"
 @views av_xa(A) =  0.5*(A[1:end-1,:].+A[2:end,:])
+"Average in y dimension"
 @views av_ya(A) =  0.5*(A[:,1:end-1].+A[:,2:end])
+
 @views function swell2h!(B, A, ndim)
     B .= B.*0.0
     if ndim==1
@@ -41,13 +49,6 @@ end
     if (ix<=size(B,1) &&    iy==1          )  B[ix,iy] =  1.5*A[ix,iy  ] - 0.5*A[ix,iy+1]  end
     if (ix<=size(B,1) &&    iy==size(B,2)  )  B[ix,iy] =  1.5*A[ix,iy-1] - 0.5*A[ix,iy-2]  end
     return 
-end
-
-@parallel function SwapRes!( dVxdτ::Data.Array, dVydτ::Data.Array, dPfdτ::Data.Array, Res_Vx::Data.Array, Res_Vy::Data.Array, Res_Pf::Data.Array )
-    @all(dVxdτ) =  @all(Res_Vx)
-    @all(dVydτ) =  @all(Res_Vy)
-    @all(dPfdτ) =  @all(Res_Pf)
-    return
 end
 
 @parallel function cum_mult2!(A1::Data.Array, A2::Data.Array, B1::Data.Array, B2::Data.Array)
@@ -117,7 +118,7 @@ end
     return
 end
 
-@parallel function compute_6!(Pf::Data.Array, Rho_X_ϕ::Data.Array, Res_Phi::Data.Array, Phi::Data.Array, Res_Pf::Data.Array, Rho_s::Data.Array, X_s::Data.Array, Phi_old::Data.Array, Rho_X_old::Data.Array, ∇V_ρ_X::Data.Array, dPfdτ::Data.Array, dampPf::Data.Number, dt_Pf::Data.Number, dtp::Data.Number)
+@parallel function compute_6!(dPfdτ::Data.Array, Pf::Data.Array, Rho_X_ϕ::Data.Array, Res_Phi::Data.Array, Phi::Data.Array, Res_Pf::Data.Array, Rho_s::Data.Array, X_s::Data.Array, Phi_old::Data.Array, Rho_X_old::Data.Array, ∇V_ρ_X::Data.Array, dampPf::Data.Number, dt_Pf::Data.Number, dtp::Data.Number)
     @all(dPfdτ)   = dampPf*@all(dPfdτ) + @all(Res_Pf)
     @inn(Pf)      = @inn(Pf) + dt_Pf*@all(dPfdτ)
     @all(Rho_X_ϕ) = (1.0-@all(Phi))*@all(Rho_s)*@all(X_s)
@@ -126,16 +127,17 @@ end
     return
 end
 
-@parallel function compute_7!(Eta::Data.Array, Lam::Data.Array, ∇V::Data.Array, ε_xx::Data.Array, ε_yy::Data.Array, ε_xy::Data.Array, Ptot::Data.Array, Vx::Data.Array, Vy::Data.Array, Phi::Data.Array, Ptot_old::Data.Array, Pf::Data.Array, Pf_old::Data.Array, RPt::Data.Array,
+macro limit_min(A,min_val) esc(:( max($Eta[$ix,$iy],$min_val) )) end
+@parallel function compute_7!(Eta::Data.Array, Lam::Data.Array, ∇V::Data.Array, ε_xx::Data.Array, ε_yy::Data.Array, ε_xy::Data.Array, Ptot::Data.Array, Vx::Data.Array, Vy::Data.Array, Phi::Data.Array, Ptot_old::Data.Array, Pf::Data.Array, Pf_old::Data.Array,
                               dtPt::Data.Number, η_m::Data.Number, ϕ_exp::Data.Number, ϕ_ini::Data.Number, λ_η::Data.Number, dtp::Data.Number, K_d::Data.Number, α::Data.Number, dx::Data.Number, dy::Data.Number)
     @all(Eta)  = η_m*exp(-ϕ_exp*(@all(Phi)-ϕ_ini))
+    @all(Eta)  = @limit_min(Eta,eta_min)
     @all(Lam)  = λ_η*@all(Eta)
     @all(∇V)   = @d_xa(Vx)/dx + @d_ya(Vy)/dy
     @all(ε_xx) = @d_xa(Vx)/dx - 1.0/3.0*@all(∇V)
     @all(ε_yy) = @d_ya(Vy)/dy - 1.0/3.0*@all(∇V)
     @all(ε_xy) = 0.5*(@d_yi(Vx)/dy + @d_xi(Vy)/dx)
-    @all(RPt)  = @all(Ptot) - (@all(Ptot_old)-dtp*( K_d*@all(∇V) - α*( (@all(Pf) - @all(Pf_old))/dtp ) - K_d*@all(Pf) / ((1.0-@all(Phi))*@all(Lam)) )) / ( 1.0 + dtp*K_d/((1.0-@all(Phi))*@all(Lam)) )
-    @all(Ptot) = @all(Ptot) - dtPt*@all(RPt)
+    @all(Ptot) = @all(Ptot) - dtPt*( @all(Ptot) - (@all(Ptot_old)-dtp*( K_d*@all(∇V) - α*( (@all(Pf) - @all(Pf_old))/dtp ) - K_d*@all(Pf) / ((1.0-@all(Phi))*@all(Lam)) )) / ( 1.0 + dtp*K_d/((1.0-@all(Phi))*@all(Lam)) ) )
     return 
 end
 
@@ -146,22 +148,22 @@ end
     return 
 end
 
-@parallel function compute_plast_1!(τII::Data.Array, Lamp::Data.Array, τ_xx::Data.Array, τ_yy::Data.Array, τ_xyn::Data.Array, σ_y::Data.Number)
+@parallel function compute_plast_1!(τII::Data.Array, LamP::Data.Array, τ_xx::Data.Array, τ_yy::Data.Array, τ_xyn::Data.Array, σ_y::Data.Number)
     @all(τII)   = sqrt( 0.5*(@all(τ_xx)*@all(τ_xx) + @all(τ_yy)*@all(τ_yy)) + @all(τ_xyn)*@all(τ_xyn) )
-    @all(Lamp)  = max(0.0, (1.0 - σ_y/@all(τII)))
+    @all(LamP)  = max(0.0, (1.0 - σ_y/@all(τII)))
     return 
 end
 
-@parallel function compute_plast_2!(τ_xx::Data.Array, τ_yy::Data.Array, τ_xy::Data.Array, Lamp::Data.Array)
-    @all(τ_xx) = (1.0-@all(Lamp))*@all(τ_xx)
-    @all(τ_yy) = (1.0-@all(Lamp))*@all(τ_yy)
-    @all(τ_xy) = (1.0- @av(Lamp))*@all(τ_xy)
+@parallel function compute_plast_2!(τ_xx::Data.Array, τ_yy::Data.Array, τ_xy::Data.Array, LamP::Data.Array)
+    @all(τ_xx) = (1.0-@all(LamP))*@all(τ_xx)
+    @all(τ_yy) = (1.0-@all(LamP))*@all(τ_yy)
+    @all(τ_xy) = (1.0- @av(LamP))*@all(τ_xy)
     return 
 end
 
-@parallel function compute_plast_3!(τII::Data.Array, Lamp2::Data.Array, τ_xx::Data.Array, τ_yy::Data.Array, τ_xyn::Data.Array, Lamp::Data.Array)
+@parallel function compute_plast_3!(τII::Data.Array, LamP2::Data.Array, τ_xx::Data.Array, τ_yy::Data.Array, τ_xyn::Data.Array, LamP::Data.Array)
     @all(τII)   = sqrt( 0.5*(@all(τ_xx)*@all(τ_xx) + @all(τ_yy)*@all(τ_yy)) + @all(τ_xyn)*@all(τ_xyn) )
-    @all(Lamp2) = @all(Lamp2) + @all(Lamp)
+    @all(LamP2) = @all(LamP2) + @all(LamP)
     return 
 end
 
@@ -171,7 +173,7 @@ end
     return 
 end
 
-@parallel function compute_10!(Vx::Data.Array, Vy::Data.Array, Res_Vx::Data.Array, Res_Vy::Data.Array, dVxdτ::Data.Array, dVydτ::Data.Array, dampV::Data.Number, dt_Stokes::Data.Number)
+@parallel function compute_10!(dVxdτ::Data.Array, dVydτ::Data.Array, Vx::Data.Array, Vy::Data.Array, Res_Vx::Data.Array, Res_Vy::Data.Array, dampV::Data.Number, dt_Stokes::Data.Number)
     @all(dVxdτ) = @all(dVxdτ)*dampV + @all(Res_Vx) # dVxdτ .= dVxdτ.*(1-Vdmp/nx) .+ Rx
     @all(dVydτ) = @all(dVydτ)*dampV + @all(Res_Vy)
     @inn(Vx)    = @inn(Vx) + dt_Stokes*@all(dVxdτ)  # Pseudo-transient form of horizontal force balance
@@ -179,15 +181,15 @@ end
     return 
 end
 
-@parallel function postproc!(drhoT_dt::Data.Array, drhosphi_dt::Data.Array, drhofphi_dt::Data.Array, drhoXphi_dt::Data.Array, dpf_dt::Data.Array, dp_dt::Data.Array, dphi_dt::Data.Array, drhos_dt::Data.Array, Rho_s::Data.Array, Rho_f::Data.Array, X_s::Data.Array, Phi::Data.Array, Rho_s_old::Data.Array, Rho_f_old::Data.Array, Rho_X_old::Data.Array, Phi_old::Data.Array, Rho_t::Data.Array, Rho_t_old::Data.Array, Pf::Data.Array, Pf_old::Data.Array, Ptot::Data.Array, Ptot_old::Data.Array, dtp::Data.Number)
-    @all(drhoT_dt)      = (@all(Rho_t)                         - @all(Rho_t_old))                     /dtp#
-    @all(drhosphi_dt)   = (@all(Rho_s)*(1.0-@all(Phi))         - @all(Rho_s_old)*(1.0-@all(Phi_old))) /dtp
-    @all(drhofphi_dt)   = (@all(Rho_f)*@all(Phi)               - @all(Rho_f_old)*@all(Phi_old))       /dtp
-    @all(drhoXphi_dt)   = (@all(Rho_s)*@all(X_s)*(1-@all(Phi)) - @all(Rho_X_old)*(1-@all(Phi_old)))   /dtp
-    @all(dpf_dt)        = (@all(Pf)                            - @all(Pf_old))                        /dtp
-    @all(dp_dt)         = (@all(Ptot)                          - @all(Ptot_old))                      /dtp
-    @all(dphi_dt)       = (@all(Phi)                           - @all(Phi_old))                       /dtp
-    @all(drhos_dt)      = (@all(Rho_s)                         - @all(Rho_s_old))                     /dtp
+@parallel function postprocess!(dRhoT_dt::Data.Array, dRhosPhi_dt::Data.Array, dRhofPhi_dt::Data.Array, dRhoXPhi_dt::Data.Array, dPf_dt::Data.Array, dPt_dt::Data.Array, dPhi_dt::Data.Array, dRhos_dt::Data.Array, Rho_s::Data.Array, Rho_f::Data.Array, X_s::Data.Array, Phi::Data.Array, Rho_s_old::Data.Array, Rho_f_old::Data.Array, Rho_X_old::Data.Array, Phi_old::Data.Array, Rho_t::Data.Array, Rho_t_old::Data.Array, Pf::Data.Array, Pf_old::Data.Array, Ptot::Data.Array, Ptot_old::Data.Array, dtp::Data.Number)
+    @all(dRhoT_dt)      = (@all(Rho_t)                         - @all(Rho_t_old))                     /dtp#
+    @all(dRhosPhi_dt)   = (@all(Rho_s)*(1.0-@all(Phi))         - @all(Rho_s_old)*(1.0-@all(Phi_old))) /dtp
+    @all(dRhofPhi_dt)   = (@all(Rho_f)*@all(Phi)               - @all(Rho_f_old)*@all(Phi_old))       /dtp
+    @all(dRhoXPhi_dt)   = (@all(Rho_s)*@all(X_s)*(1-@all(Phi)) - @all(Rho_X_old)*(1-@all(Phi_old)))   /dtp
+    @all(dPf_dt)        = (@all(Pf)                            - @all(Pf_old))                        /dtp
+    @all(dPt_dt)         = (@all(Ptot)                          - @all(Ptot_old))                      /dtp
+    @all(dPhi_dt)       = (@all(Phi)                           - @all(Phi_old))                       /dtp
+    @all(dRhos_dt)      = (@all(Rho_s)                         - @all(Rho_s_old))                     /dtp
     return
 end
 
@@ -315,7 +317,6 @@ end
     q_f_Y           = @zeros(nx  , ny-1)
     ∇q_f            = @zeros(nx-2, ny-2)
     Res_Pf          = @zeros(nx-2, ny-2)
-    Res_Pt          = @zeros(nx-0, ny-0)
     dPfdτ           = @zeros(nx-2, ny-2) # damping
     Rho_X_ϕ         = @zeros(nx  , ny  )
     Res_Phi         = @zeros(nx  , ny  )
@@ -323,17 +324,17 @@ end
     ε_yy            = @zeros(nx  , ny  )
     ε_xy            = @zeros(nx-1, ny-1)
     τII             = @zeros(nx  , ny  )
-    Lamp            = @zeros(nx  , ny  )
-    Lamp2           = @zeros(nx  , ny  )
+    LamP            = @zeros(nx  , ny  )
+    LamP2           = @zeros(nx  , ny  )
     # Arrays for post-processing
-    drhoT_dt        = @zeros(nx  , ny  ) 
-    drhosphi_dt     = @zeros(nx  , ny  ) 
-    drhofphi_dt     = @zeros(nx  , ny  ) 
-    drhoXphi_dt     = @zeros(nx  , ny  ) 
-    dpf_dt          = @zeros(nx  , ny  ) 
-    dp_dt           = @zeros(nx  , ny  ) 
-    dphi_dt         = @zeros(nx  , ny  ) 
-    drhos_dt        = @zeros(nx  , ny  ) 
+    dRhoT_dt        = @zeros(nx  , ny  ) 
+    dRhosPhi_dt     = @zeros(nx  , ny  ) 
+    dRhofPhi_dt     = @zeros(nx  , ny  ) 
+    dRhoXPhi_dt     = @zeros(nx  , ny  ) 
+    dPf_dt          = @zeros(nx  , ny  ) 
+    dPt_dt           = @zeros(nx  , ny  ) 
+    dPhi_dt         = @zeros(nx  , ny  ) 
+    dRhos_dt        = @zeros(nx  , ny  ) 
     # TMP arrays for swell2
     TmpX            = @zeros(nx+1, ny  )
     TmpY            = @zeros(nx  , ny+1)
@@ -397,7 +398,7 @@ end
             Vy         .= Data.Array( get(vars_restart, "Vy"  ,1)        )
             Vx_f       .= Data.Array( get(vars_restart, "Vx_f"  ,1)      )
             Vy_f       .= Data.Array( get(vars_restart, "Vy_f"  ,1)      ) 
-            Lamp2      .= Data.Array( get(vars_restart, "Lamp2"  ,1)     )
+            LamP2      .= Data.Array( get(vars_restart, "LamP2"  ,1)     )
             Rho_s_old  .= Data.Array( get(vars_restart, "Rho_s_old"  ,1) )
             Rho_f_old  .= Data.Array( get(vars_restart, "Rho_f_old"  ,1) )
             X_s_old    .= Data.Array( get(vars_restart, "X_s_old"  ,1)   )
@@ -445,7 +446,7 @@ end
             @parallel compute_4!(∇q_f, Res_Pf, Rho_f, Rho_s_eq, X_s_eq, q_f_X, q_f_Y, Rho_t, Rho_t_old, ∇V_ρ_t, Pf, SlopeA, rho_f_maxA, ρ_0, p_reactA, rho_s_difA, rho_s_up, rho_s_minA, x_difA, x_minA, dtp, dx, dy)
             if (itp > 1) @parallel compute_5!(X_s, Rho_s, X_s_old, X_s_eq, Rho_s_old, Rho_s_eq, dtp, kin_time) end
             # Porosity evolution
-            @parallel compute_6!(Pf, Rho_X_ϕ, Res_Phi, Phi, Res_Pf, Rho_s, X_s, Phi_old, Rho_X_old, ∇V_ρ_X, dPfdτ, dampPf, dt_Pf, dtp)
+            @parallel compute_6!(dPfdτ, Pf, Rho_X_ϕ, Res_Phi, Phi, Res_Pf, Rho_s, X_s, Phi_old, Rho_X_old, ∇V_ρ_X, dampPf, dt_Pf, dtp)
             @parallel (1:size(Pf,1)) bc_y!(Pf)
             @parallel (1:size(Pf,2)) bc_x!(Pf)
             # Stokes
@@ -457,19 +458,19 @@ end
             @parallel swell2_y!(TmpY, Rho_t)
             @parallel cum_mult2!(TmpX, TmpY, Vx, Vy)
             @parallel laplace!(∇V_ρ_t, TmpX, TmpY, dx, dy)
-            @parallel compute_7!(Eta, Lam, ∇V, ε_xx, ε_yy, ε_xy, Ptot, Vx, Vy, Phi, Ptot_old, Pf, Pf_old, Res_Pt, dt_Pt, η_m, ϕ_exp, ϕ_ini, λ_η, dtp, K_d, α, dx, dy)
+            @parallel compute_7!(Eta, Lam, ∇V, ε_xx, ε_yy, ε_xy, Ptot, Vx, Vy, Phi, Ptot_old, Pf, Pf_old, dt_Pt, η_m, ϕ_exp, ϕ_ini, λ_η, dtp, K_d, α, dx, dy)
             @parallel compute_8!(τ_xx, τ_yy, τ_xy, Eta, ε_xx, ε_yy, ε_xy)
             @parallel swell2_x!(TmpS1, τ_xy)
             @parallel swell2_y!(τ_xyn, TmpS1)
             # Plastic starts
-            @parallel compute_plast_1!(τII, Lamp, τ_xx, τ_yy, τ_xyn, σ_y)
-            @parallel compute_plast_2!(τ_xx, τ_yy, τ_xy, Lamp)
+            @parallel compute_plast_1!(τII, LamP, τ_xx, τ_yy, τ_xyn, σ_y)
+            @parallel compute_plast_2!(τ_xx, τ_yy, τ_xy, LamP)
             @parallel swell2_x!(TmpS1, τ_xy)
             @parallel swell2_y!(τ_xyn, TmpS1)
-            @parallel compute_plast_3!(τII, Lamp2, τ_xx, τ_yy, τ_xyn, Lamp)
+            @parallel compute_plast_3!(τII, LamP2, τ_xx, τ_yy, τ_xyn, LamP)
             # Plastic ends
             @parallel compute_9!(Res_Vx, Res_Vy, τ_xx, τ_yy, τ_xyn, Ptot, τ_xy, dx, dy)
-            @parallel compute_10!(Vx, Vy, Res_Vx, Res_Vy, dVxdτ, dVydτ, dampV, dt_Stokes)
+            @parallel compute_10!(dVxdτ, dVydτ, Vx, Vy, Res_Vx, Res_Vy, dampV, dt_Stokes)
             if it_tstep % nout == 0 && it_tstep > 250
                 err_Mx   = dt_Stokes*maximum(abs.(Res_Vx)/maximum(abs.(Vx)))    # Error horizontal velocitiy
                 err_R_Mx = maximum(abs.(Res_Vx))                                # Error horizontal force balance
@@ -489,9 +490,6 @@ end
                 @printf("iter = %d, error = %1.3e \n", it_tstep, err_M)
             end
         end # end PT loop
-
-        @parallel postproc!(drhoT_dt, drhosphi_dt, drhofphi_dt, drhoXphi_dt, dpf_dt, dp_dt, dphi_dt, drhos_dt, Rho_s, Rho_f, X_s, Phi, Rho_s_old, Rho_f_old, Rho_X_old, Phi_old, Rho_t, Rho_t_old, Pf, Pf_old, Ptot, Ptot_old, dtp)
-
         println("it = $(itp), time = $(round(timeP, sigdigits=3)) (time_tot = $(round(time_tot, sigdigits=3)))")
         # Visu
         if do_viz && (itp % nviz == 0 || itp == 1)
@@ -532,6 +530,7 @@ end
             #savefig("output_$(runid)_$(nx)x$(ny)/PT_HMC_Atg_$(nx)x$(ny)_$(it_viz).png")
         end
         if do_save && (itp % nsave == 0 || itp==1)
+            @parallel postprocess!(dRhoT_dt, dRhosPhi_dt, dRhofPhi_dt, dRhoXPhi_dt, dPf_dt, dPt_dt, dPhi_dt, dRhos_dt, Rho_s, Rho_f, X_s, Phi, Rho_s_old, Rho_f_old, Rho_X_old, Phi_old, Rho_t, Rho_t_old, Pf, Pf_old, Ptot, Ptot_old, dtp)
             # matwrite(string( @__DIR__, "/output_$(runid)_$(nx)x$(ny)/pt_hmc_Atg_$(itp).mat"),
             matwrite(string( @__DIR__, "/pt_hmc_Atg_", @sprintf("%04d",itp), ".mat"),
                       Dict("Ptot"=> Array(Ptot),
@@ -553,7 +552,7 @@ end
                            "Ellipse_x"=> Array(X_elli),
                            "Ellipse_y"=> Array(Y_elli),
                            "Time_vec"=> Array(Time_vec),
-                           "Lamp2"=> Array(Lamp2),
+                           "LamP2"=> Array(LamP2),
                            "Rho_s_old"=> Array(Rho_s_old),
                            "Rho_f_old"=> Array(Rho_f_old),
                            "X_s_old"=> Array(X_s_old),
@@ -563,14 +562,14 @@ end
                            "Pf_old"=> Array(Pf_old),
                            "Rho_t_old"=> Array(Rho_t_old),
                            "Lam"=> Array(Lam),
-                           "drhoT_dt"=> Array(drhoT_dt),
-                           "drhosphi_dt"=> Array(drhosphi_dt),
-                           "drhofphi_dt"=> Array(drhofphi_dt),
-                           "drhoXphi_dt"=> Array(drhoXphi_dt),
-                           "dpf_dt"=> Array(dpf_dt),
-                           "dp_dt"=> Array(dp_dt),
-                           "dphi_dt"=> Array(dphi_dt),
-                           "drhos_dt"=> Array(drhos_dt),
+                           "dRhoT_dt"=> Array(dRhoT_dt),
+                           "dRhosPhi_dt"=> Array(dRhosPhi_dt),
+                           "dRhofPhi_dt"=> Array(dRhofPhi_dt),
+                           "dRhoXPhi_dt"=> Array(dRhoXPhi_dt),
+                           "dPf_dt"=> Array(dPf_dt),
+                           "dPt_dt"=> Array(dPt_dt),
+                           "dPhi_dt"=> Array(dPhi_dt),
+                           "dRhos_dt"=> Array(dRhos_dt),
                            "Res_pf"=> Array(Res_Pf),
                            "div_qf"=> Array(∇q_f),
                            "div_rhoTvs"=> Array(∇V_ρ_t),
@@ -583,7 +582,7 @@ end
                            "timeP"=> timeP,
                            "it"=> it,
                            "it_tstep"=> it_tstep,
-                          "xc"=> Array(xc), "yc"=> Array(yc)); compress = true)
+                           "xc"=> Array(xc), "yc"=> Array(yc)); compress = true)
         end
 
     end
